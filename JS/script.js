4949c1dfdbbe555e2589153498bc052745ebbc15
rinshamol <rinshamolks65@gmail.com>
2024-08-18 14:40:46 +0530

    version 1

---
+++ JS/script.js
@@ -0,0 +1,15 @@
+function more(){
+   
+        document.getElementById('hide').style.display="block";
+        document.getElementById('tech-btn').style.display="none";
+    }
+    
+       
+
+function less(){
+    document.getElementById('hide').style.display="none";
+    document.getElementById('tech-btn').style.display="block";
+
+
+
+}
