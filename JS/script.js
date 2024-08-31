b4fc5c637433f53c498fce2a25c8883efd8dfa28
rinshamol <rinshamolks65@gmail.com>
2024-08-31 13:58:09 +0530

    final commit

--- JS/script.js
+++ JS/script.js
@@ -9,7 +9,22 @@ function more(){
 function less(){
     document.getElementById('hide').style.display="none";
     document.getElementById('tech-btn').style.display="block";
-
-
-
 }
+
+ function validation(){
+    if(document.getElementById('name').value==""){
+        document.getElementById('er1').style.display="block";
+    }else{
+        document.getElementById('er1').style.display="none";
+    }
+    if(document.getElementById('email').value==""){
+        document.getElementById('er2').style.display="block";
+    }else{
+        document.getElementById('er2').style.display="none";
+    }
+    if(document.getElementById('msg').value==""){
+        document.getElementById('er3').style.display="block";
+    }else{
+        document.getElementById('er3').style.display="none";
+    }
+ } 
